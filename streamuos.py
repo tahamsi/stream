17b5d4b959946143d678965576683be802f90df4
tahamsi <taha.msi@gmail.com>
2023-02-17 00:01:46 +0000

    Update streamuos.py

--- streamuos.py
+++ streamuos.py
@@ -102,7 +102,7 @@ def getVibrationReadings(fileName, host='127.0.0.1', port=5556):
     print("Received request from: " + str(addr))
     count=0
     while count < len(ds):
-        c_socket.send(ds.iloc[count]['Value'].to_json().encode("utf-8"))
-        print(ds.iloc[count]['Value'].to_json().encode("utf-8"))
+        c_socket.send(ds.iloc[count].value)
+        print(ds.iloc[count]['Value'].value)
         count +=1
         time.sleep(5)
